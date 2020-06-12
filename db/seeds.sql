0d57e2a8820af0f3080fa70e293a9ebac66aafa8
kmeersman <katie4682@yahoo.com>
2020-06-12 11:12:37 -0600

    Added all files to project

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name) VALUES ('BBQ, Bacon, & Cheddar Burger');
+INSERT INTO burgers (burger_name) VALUES ('Truffle Mushroom Swiss BUrger');
+INSERT INTO burgers (burger_name) VALUES ('Bacon Avocado Club Burger');
